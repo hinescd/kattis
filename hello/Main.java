0456e0531838e496ed580e66c14e38ad759b72b7
Charlie Hines <hinescd@dukes.jmu.edu>
2022-02-12 16:48:41 -0500

    Hello

---
+++ hello/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import java.math.*;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
